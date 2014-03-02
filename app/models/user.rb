c25245e7993a0cd0d40194b6082e55bd9e64a678
Elena Palmer <c.elena.palmer@gmail.com>
2014-03-02 13:19:34 -0600

    Add Password To User
    This commit adds a password field to the user model, along
    with encryption for the password.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maxiumum: 50 }
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: EMAIL_REGEX }
+  validates :email, presence: true, format: { with: EMAIL_REGEX },
+      uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }
+  has_secure_password
 end
