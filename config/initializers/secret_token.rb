bfcd86ddc1e5728bfc3877754a4a83afaaaab652
Elena Palmer <c.elena.palmer@gmail.com>
2014-03-02 10:36:36 -0600

    Initial Commit
    This is an initial commit of an empty Rails project, which
    will eventually be a guestbook website.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GuestbookApp::Application.config.secret_key_base = 'ed858777cdb852cec9d84053675ebe9ae1509967716c678829783e01595b546195f71bb0efe9668cec4c7ba3464e4318c2ffd8b5f227ffd640825484e0ae3124'
