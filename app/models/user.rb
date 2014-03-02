af5941388cd6dd94189a7ea4e9de62fdbfbc1ef0
Elena Palmer <c.elena.palmer@gmail.com>
2014-03-02 13:49:09 -0600

    Further Revisions To Users Model
    This commit adds further revisions to the users model,
    namely a bugfix for a typo.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
-  validates :name, presence: true, length: { maxiumum: 50 }
+  validates :name, presence: true, length: { maximum: 50 }
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: EMAIL_REGEX },
+  validates :email, presence: true, length: { maximum: 200 }, format: { with: EMAIL_REGEX },
       uniqueness: { case_sensitive: false }
-  validates :password, length: { minimum: 6 }
   has_secure_password
+  validates :password, length: { minimum: 6 }
 end
